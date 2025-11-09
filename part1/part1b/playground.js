68a72bd4062a3810c12bf06fd9f47cb4f4c7f6e5
febtwenty <clemens.leopold@outlook.com>
2025-11-09 21:28:41 +0100

    Course 1b

---
+++ part1/part1b/playground.js
@@ -0,0 +1,58 @@
+// variables
+const x = 1
+let y = 5
+
+console.log(x, y)
+y += 10
+console.log(x, y)
+y = 'some text'
+console.log(x, y)
+// x = 4 would yield an error, as x is constant
+
+// arrays and array map method
+const t = [1, -1, 3]
+
+t.push(5)
+
+console.log(t.length)
+console.log(t[1])
+
+t.forEach(value => {
+    console.log('t', value)
+})
+
+const t2 = t.concat(5)
+console.log(t2)
+
+const m1 = t.map(value => value * 3)
+console.log(m1)
+
+const m2 = t.map(value => '<li>' + value + '</li>')
+console.log(m2)
+
+const [first, second, ...rest] = t
+console.log(first, second)
+console.log(rest)
+
+// Objects
+const obj1 = {
+    name: 'Arturo Hellas',
+    age: 35,
+    education: 'Wise',
+}
+
+console.log(obj1)
+
+obj1['secret number'] = 123
+
+console.log(obj1['secret number'])
+console.log(obj1.name)
+
+// functions
+const sum = (p1, p2) => {
+    console.log(p1, p2)
+    return p1 + p2
+}
+
+const result = sum(2, 5)
+console.log(result)
