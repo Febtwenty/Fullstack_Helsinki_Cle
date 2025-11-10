7c4704ada3e1f590eef523885eb3b96341e7808d
febtwenty <clemens.leopold@outlook.com>
2025-11-10 11:38:28 +0100

    Course 1c final

--- part1/part1c/src/App.jsx
+++ part1/part1c/src/App.jsx
@@ -1,10 +1,33 @@
-const App = (props) => {
-  const {counter} = props
-  console.log(counter)
+import { useState } from "react"
+
+const Display = ({counter}) => <>{counter}</>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-      {counter}
+      <Display counter={counter} /> <br />
+      <Button onClick={increaseByOne} text='Plus one'/> <br />
+      <Button onClick={decreaseByOne} text='Minus one'/> <br />
+      <Button onClick={setToZero} text='Reset'/>
     </div>
   )
 }
