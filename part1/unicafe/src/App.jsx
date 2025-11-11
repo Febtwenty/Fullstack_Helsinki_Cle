18d7ba468c5709c1c8382b26449a7f856fed328b
febtwenty <clemens.leopold@outlook.com>
2025-11-11 12:03:07 +0100

    Exercise Part1.11 final unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,18 +7,35 @@ const Statistics = (props) => {
     return(<p>No feedback given.</p>)
   }
   return(
-    <>
-      <StatisticLine text='good' value={props.good} />
-      <StatisticLine text='neutral' value={props.neutral} />
-      <StatisticLine text='bad' value={props.bad} />
-      <StatisticLine text='all' value={props.all} />
-      <StatisticLine text='average' value={props.average} />
-      <StatisticLine text='positive' value={props.good * 100 / props.all} />
-    </>
+    <table>
+      <tbody>
+        <StatisticLine text='good' value={props.good} />
+        <StatisticLine text='neutral' value={props.neutral} />
+        <StatisticLine text='bad' value={props.bad} />
+        <StatisticLine text='all' value={props.all} />
+        <StatisticLine text='average' value={props.average} />
+        <StatisticLine text='positive' value={props.good * 100 / props.all} />
+      </tbody>
+    </table>
   )
 }
 
-const StatisticLine = (props) => <p>{props.text} {props.value}</p>
+const StatisticLine = (props) => {
+  if (props.text === 'positive') {
+    return(
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value} %</td>
+      </tr>
+    )
+  }
+  return(
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+  )
+}
 
 function App() {
   const [good, setGood] = useState(0)
