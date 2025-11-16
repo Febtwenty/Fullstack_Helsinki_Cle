d28171dd387c8c9a33f243b8cd1e039b933d8bd4
febtwenty <clemens.leopold@outlook.com>
2025-11-16 15:44:56 +0100

    Exercise Part2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,12 +3,13 @@ import Numbers from './components/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-1234567' },
+    { name: 'Felix Frise', number: '040-3453463' },
+    { name: 'Aldo Palermo', number: '040-867543' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const addEntry = (e) => {
     e.preventDefault()
@@ -26,19 +27,20 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      
+      <form>
+        <div>Filter shown with<input value={filter} onChange={e => setFilter(e.target.value)} /></div>
+      </form>
 
+      <h2>add a new</h2>
       <form onSubmit={addEntry}>
         <div>Name: <input value={newName} onChange={e => setNewName(e.target.value)}/></div>
         <div>Number: <input value={newNumber} onChange={e => setNewNumber(e.target.value)}/></div>
-
-        <div>
-          <button type="submit">add</button>
-        </div>
+        <div><button type="submit">add</button></div>
       </form>
 
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
-
+      <Numbers persons={persons} filter={filter}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Numbers.jsx
+++ part2/phonebook/src/components/Numbers.jsx
@@ -1,7 +1,12 @@
-const Numbers = ({ persons }) => {
+const Numbers = ({ persons, filter }) => {
+
+    const personsToShow = persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+    )
+
   return (
     <ul>
-        {persons.map(person => 
+        {personsToShow.map(person => 
             <li key={person.name}>{person.name} {person.number}</li>
         )}
     </ul>
