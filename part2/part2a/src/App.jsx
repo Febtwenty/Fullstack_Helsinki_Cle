2080cf9214cc06a8d4dcae3237da18c5707b6955
febtwenty <clemens.leopold@outlook.com>
2025-11-27 09:49:31 +0100

    Course 3b

--- part2/part2a/src/App.jsx
+++ part2/part2a/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
 import Notification from './components/Notification'
@@ -33,7 +32,7 @@ const App = () => {
   }
 
   const toggleImportanceOf = (id) => {
-    const url = `http://localhost:3001/notes/${id}`
+    const url = `http://localhost:3001/api/notes/${id}`
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important}
     noteService
