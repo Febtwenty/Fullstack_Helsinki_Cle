74f62a7fdfa2895f79c717baf55633ba95bc6588
febtwenty <clemens.leopold@outlook.com>
2025-12-01 10:58:26 +0100

    Course 3d

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -72,23 +72,19 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 // Save a new note to the DB
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
   
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 // Update a single note
@@ -123,6 +119,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
