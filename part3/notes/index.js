76b683cc31c9aa989a2455c2278bea96abf31d05
febtwenty <clemens.leopold@outlook.com>
2025-12-01 09:34:33 +0100

    Course 3c final

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -4,8 +4,20 @@ const Note = require('./models/note')
 
 const app = express()
 
-app.use(express.json())
 app.use(express.static('dist'))
+app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+app.get('/favicon.ico', (request, response) => response.status(204).end())
 
 // let notes = [
 //   {
@@ -25,29 +37,41 @@ app.use(express.static('dist'))
 //   }
 // ]
 
+// Get all notes, root
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
+// Display all notes of the DB
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
       response.json(notes)
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+// Display one particular note of the DB
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+        if (note) {
+          response.json(note)
+        } else {
+          response.status(404).end()
+        }
+      })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+// Delete a note
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+// Save a new note to the DB
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -67,24 +91,46 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+// Update a single note
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
-app.use(requestLogger)
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end
+      }
 
+      note.content = content
+      note.important = important
+
+      return note.save().then ((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+// Error Handling
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
-app.get('/favicon.ico', (request, response) => response.status(204).end())
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+// Start
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
